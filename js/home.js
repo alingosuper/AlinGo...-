31b49d6e7737ca7785fc2e9c6ecad4b4986df86c
alingosuper <alingosuper@gmail.com>
2026-02-11 17:54:16 +0500

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,1 +1,31 @@
-console.log("AlinGo Home Loaded");
+// Initialize Map
+const map = L.map('map').setView([30.3753, 69.3451], 6); // Pakistan default
+
+// OpenStreetMap Layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+// Get User Live Location
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(position => {
+
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+
+    map.setView([lat, lon], 14);
+
+    L.marker([lat, lon])
+      .addTo(map)
+      .bindPopup("You are here 📍")
+      .openPopup();
+
+  });
+}
+
+// Service Click Animation
+document.querySelectorAll(".service").forEach(item => {
+  item.addEventListener("click", () => {
+    alert(item.innerText + " Module Coming Soon");
+  });
+});
