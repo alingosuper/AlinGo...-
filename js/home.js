5f1cdc7fb991e5bafc56adebcc9d231b96949081
alingosuper <alingosuper@gmail.com>
2026-02-11 18:22:05 +0500

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,12 +1,19 @@
-// Leaflet Map
-const map = L.map('map').setView([30.3753, 69.3451], 6);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; OpenStreetMap'
-}).addTo(map);
-
 // Sidebar Toggle
 function toggleSidebar() {
-  document.getElementById("sidebar").classList.toggle("active");
-  document.getElementById("overlay").classList.toggle("active");
+  const sidebar = document.getElementById("sidebar");
+  const overlay = document.getElementById("overlay");
+
+  sidebar.classList.toggle("active");
+  overlay.classList.toggle("active");
 }
+
+// Close sidebar if clicked outside
+document.addEventListener("DOMContentLoaded", function () {
+
+  const overlay = document.getElementById("overlay");
+
+  overlay.addEventListener("click", function () {
+    toggleSidebar();
+  });
+
+});
