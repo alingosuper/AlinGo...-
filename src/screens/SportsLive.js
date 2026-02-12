3404c953beb996bf378e6b9d2506c7771db7b839
alingosuper <alingosuper@gmail.com>
2026-02-13 00:42:01 +0500

    Create SportsLive.js

---
+++ src/screens/SportsLive.js
@@ -0,0 +1,10 @@
+function SportsLive() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>Live Sports</h2>
+      <p>Sports API Integration Ready</p>
+    </div>
+  );
+}
+
+export default SportsLive;
