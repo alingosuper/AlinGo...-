f89c7e22b410cd9008b3e082de25f3f54e46bdf8
alingosuper <alingosuper@gmail.com>
2026-02-13 00:41:03 +0500

    Create WeatherDetail.js

---
+++ src/screens/WeatherDetail.js
@@ -0,0 +1,10 @@
+function WeatherDetail() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>Weather Details</h2>
+      <p>Weather API Integration Ready</p>
+    </div>
+  );
+}
+
+export default WeatherDetail;
