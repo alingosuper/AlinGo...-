8a0841c391653add29243fbce89f6a37020a399f
alingosuper <alingosuper@gmail.com>
2026-02-13 13:49:41 +0500

    Create RideMap.jsx

---
+++ src/components/RideMap.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const RideMap = () => {
+  return (
+    <MapContainer 
+      center={[30.1575, 71.5249]} 
+      zoom={13} 
+      style={{ height: "100%", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+      />
+    </MapContainer>
+  );
+};
+
+export default RideMap;
