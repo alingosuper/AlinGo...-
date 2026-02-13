97417637a6f4beebad71aeb23ad73c7b0e06ae3b
alingosuper <alingosuper@gmail.com>
2026-02-13 08:59:41 +0500

    Create SportsLive.js

---
+++ SportsLive.js
@@ -0,0 +1,10 @@
+function SportsLive() {
+  return (
+    <div className="card">
+      <h2>Live Sports Scores</h2>
+      <p>Scores API integration ready</p>
+    </div>
+  );
+}
+
+export default SportsLive;
