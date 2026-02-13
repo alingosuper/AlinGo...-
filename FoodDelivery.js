0f79c3130d654a8bd92d6e4be094fa6ab3f3d5f0
alingosuper <alingosuper@gmail.com>
2026-02-13 08:58:48 +0500

    Create FoodDelivery.js

---
+++ FoodDelivery.js
@@ -0,0 +1,10 @@
+function FoodDelivery() {
+  return (
+    <div className="card">
+      <h2>Food Delivery</h2>
+      <p>Restaurant listing and ordering system ready</p>
+    </div>
+  );
+}
+
+export default FoodDelivery;
