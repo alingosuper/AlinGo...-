b3c28782792304e1a1f73d3505f0cc61c96408c1
alingosuper <alingosuper@gmail.com>
2026-02-13 19:06:03 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        sports: resolve(__dirname, 'src/screens/SportsLive.html'),
+        ride: resolve(__dirname, 'src/screens/RideTracking.html'),
+        weather: resolve(__dirname, 'src/screens/Weather.html'),
+        profile: resolve(__dirname, 'src/screens/Profile.html'),
+      },
+    },
+  },
+});
