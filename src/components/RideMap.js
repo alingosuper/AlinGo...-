e1193a500e173a9a34609612de225c7b8f95340c
alingosuper <alingosuper@gmail.com>
2026-02-13 08:56:29 +0500

    Create RideMap.js

---
+++ src/components/RideMap.js
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+function RideMap() {
+  useEffect(() => {
+    const map = L.map("map").setView([31.5204, 74.3587], 13);
+
+    L.tileLayer(
+      "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
+      {
+        attribution: "&copy; OpenStreetMap contributors",
+      }
+    ).addTo(map);
+
+    L.marker([31.5204, 74.3587]).addTo(map).bindPopup("AlinGo Ride");
+
+    return () => map.remove();
+  }, []);
+
+  return <div id="map" style={{ height: "400px" }}></div>;
+}
+
+export default RideMap;
